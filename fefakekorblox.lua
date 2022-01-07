47538f3ffbb0e66c16dfc48eabfb4f0d5ae93509
carter <81034605+carter-fr@users.noreply.github.com>
2022-01-06 22:31:43 -0700

    Update fefakekorblox.lua

--- fefakekorblox.lua
+++ fefakekorblox.lua
@@ -1,17 +1,18 @@
 getgenv().fefakekorblox = true
-while wait() do 
+while wait(0.1) do 
     if getgenv().fefakekorblox == true then
         if game.Players.LocalPlayer.Character.Humanoid then 
              if game.Players.LocalPlayer.Character.Humanoid.RigType == Enum.HumanoidRigType.R15 then
             game.Players.LocalPlayer.Character:FindFirstChild("RightUpperLeg"):Remove()
             wait(0.1)
-            local Replacement = Instance.new("Part",game.Players.LocalPlayer.Character)
+            local Replacement = Instance.new("Part", game.Players.LocalPlayer.Character)
             Replacement.Name = "RightUpperLeg"
             Replacement.CanCollide = false
             Replacement.Transparency = 1
             else
                 game.Players.LocalPlayer.Character:FindFirstChild("Right Leg"):Remove()
-                local Replacement = Instance.new("Part",game.Players.LocalPlayer.Character)
+                wait(0.1)
+                local Replacement = Instance.new("Part", game.Players.LocalPlayer.Character)
             Replacement.Name = "Right Leg"
             Replacement.CanCollide = false
             Replacement.Transparency = 1
