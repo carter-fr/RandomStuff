38b0cf7c1c618a8d08dbe49facc19b5ec136e0db
carter <81034605+carter-fr@users.noreply.github.com>
2022-02-03 13:03:47 -0700

    Create ctrlclicktp.lua

---
+++ ctrlclicktp.lua
@@ -0,0 +1,24 @@
+--// not mine (some random guy on v3rm's from 2018)
+local UIS = game:GetService("UserInputService")
+
+local Player = game.Players.LocalPlayer
+local Mouse = Player:GetMouse()
+
+
+function GetCharacter()
+   return game.Players.LocalPlayer.Character
+end
+
+function Teleport(pos)
+   local Char = GetCharacter()
+   if Char then
+       Char:MoveTo(pos)
+   end
+end
+
+
+UIS.InputBegan:Connect(function(input)
+   if input.UserInputType == Enum.UserInputType.MouseButton1 and UIS:IsKeyDown(Enum.KeyCode.LeftControl) then
+       Teleport(Mouse.Hit.p)
+   end
+end)
