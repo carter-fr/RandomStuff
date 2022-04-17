4b2cc25efe3871f46a0dc67cc7fec4378eee46ee
carter <81034605+carter-fr@users.noreply.github.com>
2022-04-16 22:43:17 -0600

    Create carlface.lua

---
+++ carlface.lua
@@ -0,0 +1,367 @@
+-- carl face EASILY re made in like 10 minutes by carterfr#0001
+local ScreenGui = Instance.new("ScreenGui")
+local Main = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local TextLabel = Instance.new("TextLabel")
+local TextLabel_2 = Instance.new("TextLabel")
+local ScrollingFrame = Instance.new("ScrollingFrame")
+local Grandma = Instance.new("ImageButton")
+local Yum = Instance.new("ImageButton")
+local SSHF = Instance.new("ImageButton")
+local RedBeast = Instance.new("ImageButton")
+local Blizzard = Instance.new("ImageButton")
+local Radioactive = Instance.new("ImageButton")
+local Tattletale = Instance.new("ImageButton")
+local Troublemaker = Instance.new("ImageButton")
+local RedGlow = Instance.new("ImageButton")
+local Tango = Instance.new("ImageButton")
+local Playful = Instance.new("ImageButton")
+local Prankster = Instance.new("ImageButton")
+local Punk = Instance.new("ImageButton")
+local Snowflake = Instance.new("ImageButton")
+local Stitch = Instance.new("ImageButton")
+local Meanie = Instance.new("ImageButton")
+local ClassicVamp = Instance.new("ImageButton")
+local Epic = Instance.new("ImageButton")
+
+--Properties:
+
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Main.Name = "Main"
+Main.Parent = ScreenGui
+Main.Active = true
+Main.BackgroundColor3 = Color3.fromRGB(13, 13, 13)
+Main.BorderSizePixel = 0
+Main.Draggable = true
+Main.Position = UDim2.new(0.557213962, 101, 0.120901644, 88)
+Main.Size = UDim2.new(0, 254, 0, 244)
+
+UICorner.Parent = Main
+
+TextLabel.Parent = Main
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Position = UDim2.new(0.472440928, 0, 0.06967213, 0)
+TextLabel.Size = UDim2.new(0, 13, 0, 20)
+TextLabel.Font = Enum.Font.GothamBold
+TextLabel.Text = "CarlHUBv2"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextSize = 14.000
+
+TextLabel_2.Parent = Main
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.BackgroundTransparency = 1.000
+TextLabel_2.Position = UDim2.new(0.472440958, 0, 0, 0)
+TextLabel_2.Size = UDim2.new(0, 14, 0, 24)
+TextLabel_2.Font = Enum.Font.GothamBold
+TextLabel_2.Text = "Face Changer"
+TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.TextSize = 18.000
+
+ScrollingFrame.Parent = Main
+ScrollingFrame.Active = true
+ScrollingFrame.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
+ScrollingFrame.BorderSizePixel = 0
+ScrollingFrame.Position = UDim2.new(0.0254593492, 0, 0.14959012, 0)
+ScrollingFrame.Size = UDim2.new(0, 241, 0, 200)
+ScrollingFrame.CanvasSize = UDim2.new(0, 0, 1.79999995, 0)
+
+Grandma.Name = "Grandma"
+Grandma.Parent = ScrollingFrame
+Grandma.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Grandma.BorderSizePixel = 0
+Grandma.Position = UDim2.new(0.0248962659, 0, 0.0145890713, 0)
+Grandma.Size = UDim2.new(0, 65, 0, 65)
+Grandma.Image = "http://www.roblox.com/asset/?id=51241479"
+
+Yum.Name = "Yum"
+Yum.Parent = ScrollingFrame
+Yum.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Yum.BorderSizePixel = 0
+Yum.Position = UDim2.new(0.319502085, 0, 0.0145890713, 0)
+Yum.Size = UDim2.new(0, 65, 0, 65)
+Yum.Image = "http://www.roblox.com/asset/?id=26018945"
+
+SSHF.Name = "SSHF"
+SSHF.Parent = ScrollingFrame
+SSHF.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+SSHF.BorderSizePixel = 0
+SSHF.Position = UDim2.new(0.614107907, 0, 0.0145890713, 0)
+SSHF.Size = UDim2.new(0, 65, 0, 65)
+SSHF.Image = "http://www.roblox.com/asset?id=494290547"
+
+RedBeast.Name = "RedBeast"
+RedBeast.Parent = ScrollingFrame
+RedBeast.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+RedBeast.BorderSizePixel = 0
+RedBeast.Position = UDim2.new(0.0248962641, 0, 0.176246628, 0)
+RedBeast.Size = UDim2.new(0, 65, 0, 65)
+RedBeast.Image = "http://www.roblox.com/asset/?id=127959433 "
+
+Blizzard.Name = "Blizzard"
+Blizzard.Parent = ScrollingFrame
+Blizzard.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Blizzard.BorderSizePixel = 0
+Blizzard.Position = UDim2.new(0.319502085, 0, 0.176246628, 0)
+Blizzard.Size = UDim2.new(0, 65, 0, 65)
+Blizzard.Image = "http://www.roblox.com/asset/?id=209712379"
+
+Radioactive.Name = "Radioactive"
+Radioactive.Parent = ScrollingFrame
+Radioactive.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Radioactive.BorderSizePixel = 0
+Radioactive.Position = UDim2.new(0.614107907, 0, 0.176246628, 0)
+Radioactive.Size = UDim2.new(0, 65, 0, 65)
+Radioactive.Image = "rbxassetid://2225757922"
+
+Tattletale.Name = "Tattletale"
+Tattletale.Parent = ScrollingFrame
+Tattletale.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Tattletale.BorderSizePixel = 0
+Tattletale.Position = UDim2.new(0.0248962641, 0, 0.337904215, 0)
+Tattletale.Size = UDim2.new(0, 65, 0, 65)
+Tattletale.Image = "http://www.roblox.com/asset/?id=26343132"
+
+Troublemaker.Name = "Troublemaker"
+Troublemaker.Parent = ScrollingFrame
+Troublemaker.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Troublemaker.BorderSizePixel = 0
+Troublemaker.Position = UDim2.new(0.319502085, 0, 0.337904215, 0)
+Troublemaker.Size = UDim2.new(0, 65, 0, 65)
+Troublemaker.Image = "http://www.roblox.com/asset/?id=22920500"
+
+RedGlow.Name = "RedGlow"
+RedGlow.Parent = ScrollingFrame
+RedGlow.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+RedGlow.BorderSizePixel = 0
+RedGlow.Position = UDim2.new(0.614107907, 0, 0.337904215, 0)
+RedGlow.Size = UDim2.new(0, 65, 0, 65)
+RedGlow.Image = "http://www.roblox.com/asset/?id=179693472 "
+
+Tango.Name = "Tango"
+Tango.Parent = ScrollingFrame
+Tango.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Tango.BorderSizePixel = 0
+Tango.Position = UDim2.new(0.0248962641, 0, 0.499561787, 0)
+Tango.Size = UDim2.new(0, 65, 0, 65)
+Tango.Image = "http://www.roblox.com/asset/?id=629930519"
+
+Playful.Name = "Playful"
+Playful.Parent = ScrollingFrame
+Playful.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Playful.BorderSizePixel = 0
+Playful.Position = UDim2.new(0.319502085, 0, 0.499561787, 0)
+Playful.Size = UDim2.new(0, 65, 0, 65)
+Playful.Image = "rbxassetid://2409281591"
+
+Prankster.Name = "Prankster"
+Prankster.Parent = ScrollingFrame
+Prankster.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Prankster.BorderSizePixel = 0
+Prankster.Position = UDim2.new(0.614107907, 0, 0.499561787, 0)
+Prankster.Size = UDim2.new(0, 65, 0, 65)
+Prankster.Image = "http://www.roblox.com/asset/?id=20052028"
+
+Punk.Name = "Punk"
+Punk.Parent = ScrollingFrame
+Punk.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Punk.BorderSizePixel = 0
+Punk.Position = UDim2.new(0.0248962641, 0, 0.661219418, 0)
+Punk.Size = UDim2.new(0, 65, 0, 65)
+Punk.Image = "http://www.roblox.com/asset/?id=387256104 "
+
+Snowflake.Name = "Snowflake"
+Snowflake.Parent = ScrollingFrame
+Snowflake.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Snowflake.BorderSizePixel = 0
+Snowflake.Position = UDim2.new(0.319502085, 0, 0.661219418, 0)
+Snowflake.Size = UDim2.new(0, 65, 0, 65)
+Snowflake.Image = "rbxassetid://1213444061"
+
+Stitch.Name = "Stitch"
+Stitch.Parent = ScrollingFrame
+Stitch.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Stitch.BorderSizePixel = 0
+Stitch.Position = UDim2.new(0.614107907, 0, 0.661219418, 0)
+Stitch.Size = UDim2.new(0, 65, 0, 65)
+Stitch.Image = "http://www.roblox.com/asset/?id=8329438"
+
+Meanie.Name = "Meanie"
+Meanie.Parent = ScrollingFrame
+Meanie.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Meanie.BorderSizePixel = 0
+Meanie.Position = UDim2.new(0.0248962641, 0, 0.82287699, 0)
+Meanie.Size = UDim2.new(0, 65, 0, 65)
+Meanie.Image = "http://www.roblox.com/asset/?id=508490451"
+
+ClassicVamp.Name = "ClassicVamp"
+ClassicVamp.Parent = ScrollingFrame
+ClassicVamp.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ClassicVamp.BorderSizePixel = 0
+ClassicVamp.Position = UDim2.new(0.319502085, 0, 0.82287699, 0)
+ClassicVamp.Size = UDim2.new(0, 65, 0, 65)
+ClassicVamp.Image = "http://www.roblox.com/asset/?id=7074827"
+
+Epic.Name = "Epic"
+Epic.Parent = ScrollingFrame
+Epic.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Epic.BorderSizePixel = 0
+Epic.Position = UDim2.new(0.614107907, 0, 0.82287699, 0)
+Epic.Size = UDim2.new(0, 65, 0, 65)
+Epic.Image = "http://www.roblox.com/asset/?id=42070872"
+
+-- Scripts:
+
+local function DQUXEA_fake_script() -- Grandma.LocalScript 
+	local script = Instance.new('LocalScript', Grandma)
+
+	script.Parent.Activated:Connect(function()
+		game.Players.LocalPlayer.Character.Head.face.Texture = tostring(script.Parent.Image)
+	end)
+end
+coroutine.wrap(DQUXEA_fake_script)()
+local function UKDTF_fake_script() -- Yum.LocalScript 
+	local script = Instance.new('LocalScript', Yum)
+
+	script.Parent.Activated:Connect(function()
+		game.Players.LocalPlayer.Character.Head.face.Texture = tostring(script.Parent.Image)
+	end)
+end
+coroutine.wrap(UKDTF_fake_script)()
+local function OPDWWSH_fake_script() -- SSHF.LocalScript 
+	local script = Instance.new('LocalScript', SSHF)
+
+	script.Parent.Activated:Connect(function()
+		game.Players.LocalPlayer.Character.Head.face.Texture = tostring(script.Parent.Image)
+	end)
+end
+coroutine.wrap(OPDWWSH_fake_script)()
+local function ZMIQ_fake_script() -- RedBeast.LocalScript 
+	local script = Instance.new('LocalScript', RedBeast)
+
+	script.Parent.Activated:Connect(function()
+		game.Players.LocalPlayer.Character.Head.face.Texture = tostring(script.Parent.Image)
+	end)
+end
+coroutine.wrap(ZMIQ_fake_script)()
+local function VTAVYV_fake_script() -- Blizzard.LocalScript 
+	local script = Instance.new('LocalScript', Blizzard)
+
+	script.Parent.Activated:Connect(function()
+		game.Players.LocalPlayer.Character.Head.face.Texture = tostring(script.Parent.Image)
+	end)
+end
+coroutine.wrap(VTAVYV_fake_script)()
+local function HLXC_fake_script() -- Radioactive.LocalScript 
+	local script = Instance.new('LocalScript', Radioactive)
+
+	script.Parent.Activated:Connect(function()
+		game.Players.LocalPlayer.Character.Head.face.Texture = tostring(script.Parent.Image)
+	end)
+end
+coroutine.wrap(HLXC_fake_script)()
+local function AKSMHO_fake_script() -- Tattletale.LocalScript 
+	local script = Instance.new('LocalScript', Tattletale)
+
+	script.Parent.Activated:Connect(function()
+		game.Players.LocalPlayer.Character.Head.face.Texture = tostring(script.Parent.Image)
+	end)
+end
+coroutine.wrap(AKSMHO_fake_script)()
+local function DUDQS_fake_script() -- Troublemaker.LocalScript 
+	local script = Instance.new('LocalScript', Troublemaker)
+
+	script.Parent.Activated:Connect(function()
+		game.Players.LocalPlayer.Character.Head.face.Texture = tostring(script.Parent.Image)
+	end)
+end
+coroutine.wrap(DUDQS_fake_script)()
+local function IRDSF_fake_script() -- RedGlow.LocalScript 
+	local script = Instance.new('LocalScript', RedGlow)
+
+	script.Parent.Activated:Connect(function()
+		game.Players.LocalPlayer.Character.Head.face.Texture = tostring(script.Parent.Image)
+	end)
+end
+coroutine.wrap(IRDSF_fake_script)()
+local function RPNWE_fake_script() -- Tango.LocalScript 
+	local script = Instance.new('LocalScript', Tango)
+
+	script.Parent.Activated:Connect(function()
+		game.Players.LocalPlayer.Character.Head.face.Texture = tostring(script.Parent.Image)
+	end)
+end
+coroutine.wrap(RPNWE_fake_script)()
+local function VRFT_fake_script() -- Playful.LocalScript 
+	local script = Instance.new('LocalScript', Playful)
+
+	script.Parent.Activated:Connect(function()
+		game.Players.LocalPlayer.Character.Head.face.Texture = tostring(script.Parent.Image)
+	end)
+end
+coroutine.wrap(VRFT_fake_script)()
+local function ZKUIXU_fake_script() -- Prankster.LocalScript 
+	local script = Instance.new('LocalScript', Prankster)
+
+	script.Parent.Activated:Connect(function()
+		game.Players.LocalPlayer.Character.Head.face.Texture = tostring(script.Parent.Image)
+	end)
+end
+coroutine.wrap(ZKUIXU_fake_script)()
+local function URVCKQT_fake_script() -- Punk.LocalScript 
+	local script = Instance.new('LocalScript', Punk)
+
+	script.Parent.Activated:Connect(function()
+		game.Players.LocalPlayer.Character.Head.face.Texture = tostring(script.Parent.Image)
+	end)
+end
+coroutine.wrap(URVCKQT_fake_script)()
+local function BACA_fake_script() -- Snowflake.LocalScript 
+	local script = Instance.new('LocalScript', Snowflake)
+
+	script.Parent.Activated:Connect(function()
+		game.Players.LocalPlayer.Character.Head.face.Texture = tostring(script.Parent.Image)
+	end)
+end
+coroutine.wrap(BACA_fake_script)()
+local function HAXZRNM_fake_script() -- Stitch.LocalScript 
+	local script = Instance.new('LocalScript', Stitch)
+
+	script.Parent.Activated:Connect(function()
+		game.Players.LocalPlayer.Character.Head.face.Texture = tostring(script.Parent.Image)
+	end)
+end
+coroutine.wrap(HAXZRNM_fake_script)()
+local function QOWBK_fake_script() -- Meanie.LocalScript 
+	local script = Instance.new('LocalScript', Meanie)
+
+	script.Parent.Activated:Connect(function()
+		game.Players.LocalPlayer.Character.Head.face.Texture = tostring(script.Parent.Image)
+	end)
+end
+coroutine.wrap(QOWBK_fake_script)()
+local function GAZLBR_fake_script() -- ClassicVamp.LocalScript 
+	local script = Instance.new('LocalScript', ClassicVamp)
+
+	script.Parent.Activated:Connect(function()
+		game.Players.LocalPlayer.Character.Head.face.Texture = tostring(script.Parent.Image)
+	end)
+end
+coroutine.wrap(GAZLBR_fake_script)()
+local function IDRPILK_fake_script() -- Epic.LocalScript 
+	local script = Instance.new('LocalScript', Epic)
+
+	script.Parent.Activated:Connect(function()
+		game.Players.LocalPlayer.Character.Head.face.Texture = tostring(script.Parent.Image)
+	end)
+end
+coroutine.wrap(IDRPILK_fake_script)()
+local function VEEWQ_fake_script() -- ScreenGui.LocalScript 
+	local script = Instance.new('LocalScript', ScreenGui)
+
+	script.Parent.Main.Draggable = true
+	script.Parent.Main.Active = true
+end
+coroutine.wrap(VEEWQ_fake_script)()
